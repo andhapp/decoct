c1cac52cffe0a240a1cc249d9cda12384d6e7d1d
Anuj Dutta <dutta.anuj@gmail.com>
2009-04-24 19:09:35 +0100

    Fixed a bug in the generic app class

--- lib/templates/generic_app.rb
+++ lib/templates/generic_app.rb
@@ -3,5 +3,5 @@ $LOAD_PATH.unshift File.dirname(__FILE__) + '/../sinatra/lib'
 %w{rubygems sinatra}.each {|x| require x}
 
 get '/' do
-  'Congratulations!!! You have successfully installed decoct.'
+  'It works!!!'
 end
