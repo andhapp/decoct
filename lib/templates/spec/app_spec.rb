38e3ec70a758c161f7d736ec87d49dbeac7daff7
anuj  dutta <andhapp@andHapps-MacBook.local>
2009-07-12 01:19:18 +0530

    Replaced sinatra test with rack test as the former has been deprecated

--- lib/templates/spec/app_spec.rb
+++ lib/templates/spec/app_spec.rb
@@ -1,15 +1,19 @@
-require "spec"
-require "spec/interop/test"
-require "rack/test"
+%w{spec spec/interop/test rack/test}.each {|x| require x}
 
 set :environment, :test
 
 describe 'App' do
-  include Rack::Test::Methods
 
+  include Rack::Test::Methods
+  
+  def app
+    Sinatra::Application
+  end
+  
   it "should pick the spec files for autotest and display error messages with growl" do
     get '/'
     last_response.should be_ok
     last_response.body.should == 'It works!!!'
   end
+
 end
