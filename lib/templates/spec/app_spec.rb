09d71a3188bff3ab57c2c5cf93bf221e17f5ff82
anuj  dutta <andhapp@andHapps-MacBook.local>
2009-11-08 05:18:09 +0530

    Misleading test name changed it to reflect the intent better

--- lib/templates/spec/app_spec.rb
+++ lib/templates/spec/app_spec.rb
@@ -1,5 +1,5 @@
 describe 'App' do  
-  it "should pick the spec files for autotest and display error messages with growl" do
+  it "should pick the main app file and run the spec" do
     get '/'
     last_response.should be_ok
     last_response.body.should == 'It works!!!'
